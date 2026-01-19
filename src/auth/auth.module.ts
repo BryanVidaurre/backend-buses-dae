5cec1bcb47b4b09304c7fdf3aed077088921bb98
Bryan J. Vidaurre <119612480+BryanVidaurre@users.noreply.github.com>
2026-01-19 12:10:51 -0300

    .env jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AdminUser } from './entities/admin-user.entity';
@@ -8,12 +10,19 @@ import { AdminUser } from './entities/admin-user.entity';
 @Module({
   imports: [
     TypeOrmModule.forFeature([AdminUser]),
-    JwtModule.register({
-      secret: 'CLAVE_SUPER_SECRETA',
-      signOptions: { expiresIn: '8h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET')!,
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN', '8h') as any,
+        },
+      }),
     }),
   ],
   providers: [AuthService],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
